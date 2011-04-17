69f2910af3a7573b3d4601a1563138219d565506
Xeli <Xeli@localhost>
2011-04-18 01:07:17 +0200

    Clean up directories..

--- Java-To-Pascal/jni/makeandroid.bat
+++
@@ -1,7 +0,0 @@
-SET PATH=d:\FPC\2.5.1\bin\i386-win32\;d:\FPC\2.5.1\bin\arm-linux\
-SET nodosfilewarning=1
-ppcrossarm -b -B -XX -Xc -XD -Tlinux -darm -dandroid -dBeRoXMRingBuffer -dMixerOwnDefine -dUnrolledLoops -dMixerNearest -dMixerLinear -dMixerCubicSpline -dUseMIDIMacrors -dUseAmigaMODLoader -dUseADPCM4 -dUseFilters -dUseMono -dUseStereo -dUseSaver -olibBeRoXM.so libBeRoXM.dpr
-rem ppcrossarm -b -B -XX -Xc -XD -Tlinux -darm -dandroid -dBeRoXMRingBuffer -dMixerOwnDefine -dUnrolledLoops -dMixerNearest -dMixerLinear -dMixerCubicSpline -dMixerWindowedFIR -dUseMIDIMacrors -dUseAmigaMODLoader -dUseADPCM4 -dUseFilters -dUseMono -dUseStereo -dUseSaver -olibBeRoXM.so libBeRoXM.dpr
-
-
-
